2b066d6786246d71fe7134b0f4ab60a4a50acdf4
Irfan Ali <alimohammadirfan7@gmail.com>
2023-04-11 20:15:16 +0600

    job details section done

--- src/Component/JobDetails/JobDetails.jsx
+++ src/Component/JobDetails/JobDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useLoaderData, useParams } from 'react-router-dom';
+import { Link, useLoaderData, useParams } from 'react-router-dom';
 
 const JobDetails = () => {
      const jobs = useLoaderData();
@@ -14,8 +14,29 @@ const JobDetails = () => {
        },[])
        
     return (
-        <div>
-              <p>{jobdetails.location}</p>
+        <div className='flex'>
+              <div>
+               <p>Job Description : {jobdetails.description} </p>
+               <p>Job Responsibility : {jobdetails.job_responsibility} </p>
+               <p>Educational Requirements:
+                 {jobdetails.education_requirement} 
+                 </p>
+               <p>Experiences :
+                 {jobdetails.experiences} Years in this field 
+                 </p>
+              </div>
+              <div className='bg-gray-200 p-7'>
+                 <p>Job Details</p>
+                  <p>Salary: {jobdetails.salary}</p>
+                  <p>Job Title: {jobdetails.title}</p>
+                   <p>Contact Information</p>
+                    <p>Phone : {jobdetails.phone}</p>
+                    <p>Email : {jobdetails.email}</p>
+                    <p>Address: {jobdetails.location}</p>
+                    <div>
+                        <Link to={"/applyjob"}>Apply Now</Link>
+                    </div>
+              </div>
         </div>
     );
 };
