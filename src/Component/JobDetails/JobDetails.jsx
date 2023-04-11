7102798978bd8a6461635e18574a94a9ec41d694
Irfan Ali <alimohammadirfan7@gmail.com>
2023-04-11 23:10:14 +0600

    job details page design done

--- src/Component/JobDetails/JobDetails.jsx
+++ src/Component/JobDetails/JobDetails.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useLoaderData, useParams } from 'react-router-dom';
+import './JobDetails.css'
 
 const JobDetails = () => {
      const jobs = useLoaderData();
@@ -14,28 +15,27 @@ const JobDetails = () => {
        },[])
        
     return (
-        <div className='flex'>
+        <div className='flex p-32'>
               <div>
-               <p>Job Description : {jobdetails.description} </p>
-               <p>Job Responsibility : {jobdetails.job_responsibility} </p>
-               <p>Educational Requirements:
+               <p className='mb-5 text-xl'><span className='font-bold text-2xl'>Job Description : </span> {jobdetails.description} </p>
+               <p className='mb-5 text-xl'> <span className='font-bold text-2xl'>Job Responsibility : </span>{jobdetails.job_responsibility} </p>
+               <p className='mb-5 text-xl'> <span className='font-bold text-2xl'> Educational Requirements:</span>
                  {jobdetails.education_requirement} 
                  </p>
-               <p>Experiences :
+               <p className='mb-5 text-xl'><span className='font-bold text-2xl'>Experiences : </span>
                  {jobdetails.experiences} Years in this field 
                  </p>
               </div>
-              <div className='bg-gray-200 p-7'>
-                 <p>Job Details</p>
+              <div className='bg-gray-200 p-20 w-96 '>
+                 <p className='font-bold underline mb-2'>Job Details</p>
                   <p>Salary: {jobdetails.salary}</p>
                   <p>Job Title: {jobdetails.title}</p>
-                   <p>Contact Information</p>
+                   <p className='font-bold  underline mb-2'>Contact Information</p>
                     <p>Phone : {jobdetails.phone}</p>
                     <p>Email : {jobdetails.email}</p>
-                    <p>Address: {jobdetails.location}</p>
-                    <div>
-                        <Link to={"/applyjob"}>Apply Now</Link>
-                    </div>
+                    <p className='mb-7'>Address: {jobdetails.location}</p>
+                        <Link to={"/applyjob"} id='header-button' className='p-3 ml-7'>Apply Now</Link>
+                    
               </div>
         </div>
     );
